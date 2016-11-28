251d2a38f7ed13b0de989eb27768e75501305d88
Matheus Martins <matheusmcrds@gmail.com>
2016-11-28 19:54:38 -0300

    first commit

---
+++ DIOCollectionViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DIOCollectionViewExample
+//
+//  Created by Matheus Martins on 11/28/16.
+//  Copyright © 2016 matheusmcardoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
